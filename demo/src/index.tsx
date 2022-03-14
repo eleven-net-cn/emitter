302794910d03e0837261bab8fcf0146995de0274
Eleven <jun.xi@ximalaya.com>
2022-03-14 17:42:54 +0800

    chore: 验收 v1.0.2

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,14 +1,15 @@
 import '@eleven.fe/reset.css';
 import React, { useEffect } from 'react';
 import { render } from 'react-dom';
-import Emitter from '../../src/index';
-// import Emitter from '@e.fe/emitter';
+// import Emitter from '../../src/index';
+import Emitter from '@e.fe/emitter';
 
 const emitter = new Emitter();
 
 const App = () => {
   useEffect(() => {
-    emitter.on('custom-event', msg => {
+    emitter.on('custom-event', (msg, num) => {
+      console.log('num: ', num);
       alert(`fire custom-event, and say ${msg}`);
     });
   }, []);
@@ -17,7 +18,7 @@ const App = () => {
     <div>
       <button
         onClick={() => {
-          emitter.emit('custom-event', 'hello~');
+          emitter.emit('custom-event', 'hello~', 123);
         }}
       >
         Click Here
